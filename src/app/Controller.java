e709322bdd24779c324e7f181722f2846629425b
Erick R <erk.rivs@gmail.com>
2021-10-06 22:27:54 -0400

    Initial Commit: Creating app package and Controller Class and created Welcome and Option Screen.

---
+++ src/app/Controller.java
@@ -0,0 +1,19 @@
+package app;
+
+public class Controller {
+    public static void main(String[] args) {
+
+        //Display Welcome & Main Menu
+        System.out.println("-----------------------------------------");
+        System.out.println("\t\tWelcome to Bank of NYC");
+        System.out.println("-----------------------------------------");
+        System.out.println("Please Select an Option Below:");
+        System.out.println("""
+                1 - Account Information
+                2 - Setup a New Account
+                3 - Deposit Money
+                4 - Withdraw Money
+                """);
+        System.out.println("Option: ");
+    }
+}
